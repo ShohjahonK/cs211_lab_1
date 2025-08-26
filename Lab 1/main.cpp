353cd624651d547883570b236ec506c7810667de
ShohjahonK <s.karimberganov@newuu.uz>
2025-08-26 09:26:23 +0500

    Initial Commit

---
+++ Lab 1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab 1
+//
+//  Created by Shohjahon Karimberganov on 8/26/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, Worldd!\n";
+    return 0;
+}
